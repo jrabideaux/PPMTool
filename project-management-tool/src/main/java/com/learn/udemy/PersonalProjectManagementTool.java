e83d22efea493d36372832d48fbc584cd5de0ab5
Josh Rabideaux <>
2020-02-05 17:27:02 -0600

    first commit

---
+++ project-management-tool/src/main/java/com/learn/udemy/PersonalProjectManagementTool.java
@@ -0,0 +1,13 @@
+package com.learn.udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementTool {
+
+    public static void main (String[] args) {
+
+        SpringApplication.run(PersonalProjectManagementTool.class, args);
+    }
+}
